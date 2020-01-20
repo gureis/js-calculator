6a145cbed1faa95094fd179ad81169e428badee6
Gustavo Reis <gustavo.reis@creditas.com.br>
2020-01-20 15:47:38 -0300

    implements mul, sub, div functions

--- js/script.js
+++ js/script.js
@@ -1,32 +1,52 @@
 let display = document.querySelector(".display");
 let leftOperand = null;
 let rightOperand = null;
-let result = 0;
-let hasDot = false;
+let result = null;
 let operation = null;
+let hasDot = false;
+let isNewNumber = false;
+let isSoftClear = false;
 
 function writeNumber(value) {
-    let newValue = display.innerHTML + value;
-    display.innerHTML = newValue.replace(/^0+/, '');
+    let newValue = "";
+    if(isNewNumber) {
+        display.innerHTML = value;
+        newValue = value;
+        isNewNumber = false;
+    } else {
+        newValue = display.innerHTML + value;
+        display.innerHTML = newValue.replace(/^0+/, '');
+    }
     if (operation === null) {
         leftOperand = newValue;
     } else {
         rightOperand = newValue;
     }
+    isSoftClear = false;
 }
 
 function clearDisplay() {
-    leftOperand = null;
-    rightOperand = null;
-    result = 0;
-    hasDot = false;
-    operation = null
+    if(isSoftClear) {
+        leftOperand = null;
+        rightOperand = null;
+        result = null;
+        hasDot = false;
+        operation = null;
+    } else {
+        rightOperand = 0;
+    }
+    isSoftClear = !isSoftClear;
     display.innerHTML = "0";
 }
 
 function setOperation(localOperation) {
-    operation = localOperation;
     display.innerHTML = '0';
+    hasDot = false;
+    operation = localOperation;
+    if(rightOperand) {
+        executeOperation();
+        isNewNumber = true;
+    }
 }
 
 function executeOperation() {
@@ -37,27 +57,42 @@ function executeOperation() {
             add();
             break;
         case 'SUB':
+            sub();
             break;
         case 'DIV':
-
+            div();
             break;
         case 'MUL':
-
+            mul();
             break;
         default:
             console.log('Operação não existente');
             break;
     }
+    leftOperand = result;
+    display.innerHTML = Number(result.toFixed(8));
 }
 
 function add() {
     result = leftOperand + rightOperand;
-    display.innerHTML = result;
-    leftOperand = result;
+}
+
+function sub() {
+    result = leftOperand - rightOperand;
+
+}
+
+function div() {
+    result = leftOperand / rightOperand;
+}
+
+function mul() {
+    result = leftOperand * rightOperand;
 }
 
 function writeDot() {
     if (!hasDot) {
+        display.innerHTML = display.innerHTML.replace(/^0+/, '');
         display.innerHTML += ".";
         hasDot = true;
     }
